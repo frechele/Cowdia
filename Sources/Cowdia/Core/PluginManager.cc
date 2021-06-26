dabed8c7a39df0989296de3e69d7836e4c5cd312
JYPark09 <jyp10987@gmail.com>
2021-06-26 16:18:30 +0900

    feat(core): create PluginManager class

---
+++ Sources/Cowdia/Core/PluginManager.cc
@@ -0,0 +1,33 @@
+#include <Cowdia/Core/PluginManager.hpp>
+
+#include <algorithm>
+
+namespace Cowdia::Core
+{
+PluginManager::~PluginManager()
+{
+    for (auto& pr : plugins_)
+        pr.second->Unload();
+}
+
+Plugin* PluginManager::Load(const std::string& name)
+{
+    const auto it = plugins_.find(name);
+    if (it != end(plugins_))
+        return it->second.get();
+
+    plugins_[name] = std::make_unique<Plugin>(name);
+
+    return plugins_[name].get();
+}
+
+void PluginManager::Unload(const std::string& name)
+{
+    const auto it = plugins_.find(name);
+    if (it == end(plugins_))
+        return;
+
+    it->second->Unload();
+    plugins_.erase(it);
+}
+}  // namespace Cowdia::Core
