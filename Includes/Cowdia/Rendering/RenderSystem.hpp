646e58d689da8ccbd02ce2d17446a8e26a7ccff7
JYPark09 <jyp10987@gmail.com>
2021-06-30 11:16:20 +0900

    feat(core): create RenderSystem class

---
+++ Includes/Cowdia/Rendering/RenderSystem.hpp
@@ -0,0 +1,35 @@
+#ifndef COWDIA_RENDER_SYSTEM_HPP
+#define COWDIA_RENDER_SYSTEM_HPP
+
+#include <Cowdia/Core/Common.hpp>
+#include <Cowdia/Rendering/RenderWindow.hpp>
+
+#include <string>
+
+namespace Cowdia::Rendering
+{
+class COWDIA_API RenderSystem
+{
+ public:
+    //! Default destructor
+    virtual ~RenderSystem() = default;
+
+    //! Initialize render system.
+    virtual void Initialize() = 0;
+
+    //! Shutdown render system.
+    virtual void Shutdown() = 0;
+
+    //! Returns the name of render system.
+    [[nodiscard]] virtual std::string GetName() const = 0;
+
+    //! Returns render window instance.
+    [[nodiscard]] virtual RenderWindow* GetRenderWindow() = 0;
+
+    //! Poll events.
+    //! \return true if there is events to process else false.
+    [[nodiscard]] virtual bool PollEvents() = 0;
+};
+}  // namespace Cowdia::Rendering
+
+#endif  // COWDIA_RENDER_SYSTEM_HPP
