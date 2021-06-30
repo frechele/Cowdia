b3b1aa8ab3a16d16fd03ebe65c63a8911ac7b515
JYPark09 <jyp10987@gmail.com>
2021-06-30 11:16:49 +0900

    feat(dx12): implement basic render system

---
+++ Extensions/D3D12Renderer/D3D12RenderSystem.hpp
@@ -0,0 +1,31 @@
+#ifndef D3D12RS_RENDER_SYSTEM_HPP
+#define D3D12RS_RENDER_SYSTEM_HPP
+
+#include "D3D12Common.hpp"
+
+#include <Cowdia/Rendering/RenderSystem.hpp>
+#include <Cowdia/Rendering/RenderWindow.hpp>
+
+#include <Windows.h>
+
+namespace Cowdia::Rendering
+{
+class COWDIA_D3D12_API D3D12RenderSystem final : public RenderSystem
+{
+ public:
+    void Initialize() override;
+    void Shutdown() override;
+
+    [[nodiscard]] std::string GetName() const override;
+
+    [[nodiscard]] RenderWindow* GetRenderWindow() override;
+
+    [[nodiscard]] bool PollEvents() override;
+
+ private:
+    HINSTANCE hInstance_{ nullptr };
+    RenderWindow* renderWindow_{ nullptr };
+};
+}
+
+#endif  // D3D12RS_RENDER_SYSTEM_HPP
---
+++ Extensions/D3D12Renderer/D3D12RenderWindow.hpp
@@ -0,0 +1,35 @@
+#ifndef D3D12RS_RENDER_WINDOW_HPP
+#define D3D12RS_RENDER_WINDOW_HPP
+
+#include "D3D12Common.hpp"
+
+#include <Cowdia/Rendering/RenderWindow.hpp>
+
+#include <Windows.h>
+
+namespace Cowdia::Rendering
+{
+class COWDIA_D3D12_API D3D12RenderWindow final : public RenderWindow
+{
+ public:
+    D3D12RenderWindow(HINSTANCE hInstance);
+
+    bool Create(int width, int height, bool fullcscreen) override;
+    void Destroy() override;
+
+    void SetTitle(const std::string& title) override;
+
+    void Resize(int width, int height) override;
+
+    void Fullscreen(bool fullscreen) override;
+
+ private:
+    static LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+ private:
+    HINSTANCE hInstance_;
+    HWND hwnd_{ nullptr };
+};
+}  // namespace Cowdia::Rendering
+
+#endif  // D3D12RS_RENDER_WINDOW_HPP
