650457f6b21327d9afba50a8137d4ad205512768
JYPark09 <jyp10987@gmail.com>
2021-08-08 11:00:17 +0900

    test: add unittests for Vector class

--- Tests/UnitTests/Math/VectorTests.cc
+++ Tests/UnitTests/Math/VectorTests.cc
@@ -13,6 +13,10 @@ TEST_CASE("[Math/Vector2] - Basic Arithmetic Operators")
     Math::Vector2 A{ 1.f, 2.f };
     Math::Vector2 B{ 3.f, 4.f };
 
+    CHECK_EQ(A.Dim(), 2);
+    REQUIRE(B.Length() == doctest::Approx(5.f).epsilon(eps));
+    REQUIRE(A.Dot(B) == doctest::Approx(11.f).epsilon(eps));
+
     REQUIRE(A.X() == doctest::Approx(1.f).epsilon(eps));
     REQUIRE(A.Y() == doctest::Approx(2.f).epsilon(eps));
     REQUIRE(B.X() == doctest::Approx(3.f).epsilon(eps));
@@ -50,6 +54,10 @@ TEST_CASE("[Math/Vector3] - Basic Arithmetic Operators")
     Math::Vector3 A{ 1.f, 2.f, 3.f };
     Math::Vector3 B{ 4.f, 5.f, 6.f };
 
+    CHECK_EQ(A.Dim(), 3);
+    REQUIRE(B.Length() == doctest::Approx(std::sqrt(77.f)).epsilon(eps));
+    REQUIRE(A.Dot(B) == doctest::Approx(32.f).epsilon(eps));
+
     REQUIRE(A.X() == doctest::Approx(1.f).epsilon(eps));
     REQUIRE(A.Y() == doctest::Approx(2.f).epsilon(eps));
     REQUIRE(A.Z() == doctest::Approx(3.f).epsilon(eps));
@@ -86,4 +94,61 @@ TEST_CASE("[Math/Vector3] - Basic Arithmetic Operators")
     REQUIRE(Adiv.X() == doctest::Approx(0.5f).epsilon(eps));
     REQUIRE(Adiv.Y() == doctest::Approx(1.f).epsilon(eps));
     REQUIRE(Adiv.Z() == doctest::Approx(1.5f).epsilon(eps));
+}
+
+TEST_CASE("[Math/Vector4] - Basic Arithmetic Operators")
+{
+    constexpr float eps = 1e-5f;
+
+    Math::Vector4 A{ 1.f, 2.f, 3.f, 4.f };
+    Math::Vector4 B{ 5.f, 6.f, 7.f, 8.f };
+
+    CHECK_EQ(A.Dim(), 4);
+    REQUIRE(B.Length() == doctest::Approx(std::sqrt(174.f)).epsilon(eps));
+    REQUIRE(A.Dot(B) == doctest::Approx(70.f).epsilon(eps));
+
+    REQUIRE(A.X() == doctest::Approx(1.f).epsilon(eps));
+    REQUIRE(A.Y() == doctest::Approx(2.f).epsilon(eps));
+    REQUIRE(A.Z() == doctest::Approx(3.f).epsilon(eps));
+    REQUIRE(A.W() == doctest::Approx(4.f).epsilon(eps));
+    REQUIRE(B.X() == doctest::Approx(5.f).epsilon(eps));
+    REQUIRE(B.Y() == doctest::Approx(6.f).epsilon(eps));
+    REQUIRE(B.Z() == doctest::Approx(7.f).epsilon(eps));
+    REQUIRE(B.W() == doctest::Approx(8.f).epsilon(eps));
+
+    Math::Vector4 AplusB = A + B;
+    REQUIRE(AplusB.X() == doctest::Approx(6.f).epsilon(eps));
+    REQUIRE(AplusB.Y() == doctest::Approx(8.f).epsilon(eps));
+    REQUIRE(AplusB.Z() == doctest::Approx(10.f).epsilon(eps));
+    REQUIRE(AplusB.W() == doctest::Approx(12.f).epsilon(eps));
+
+    Math::Vector4 BminusA = B - A;
+    REQUIRE(BminusA.X() == doctest::Approx(4.f).epsilon(eps));
+    REQUIRE(BminusA.Y() == doctest::Approx(4.f).epsilon(eps));
+    REQUIRE(BminusA.Z() == doctest::Approx(4.f).epsilon(eps));
+    REQUIRE(BminusA.W() == doctest::Approx(4.f).epsilon(eps));
+
+    Math::Vector4 Aplus2 = A + 2;
+    REQUIRE(Aplus2.X() == doctest::Approx(3.f).epsilon(eps));
+    REQUIRE(Aplus2.Y() == doctest::Approx(4.f).epsilon(eps));
+    REQUIRE(Aplus2.Z() == doctest::Approx(5.f).epsilon(eps));
+    REQUIRE(Aplus2.W() == doctest::Approx(6.f).epsilon(eps));
+
+    Math::Vector4 Aminus2 = A - 2;
+    REQUIRE(Aminus2.X() == doctest::Approx(-1.f).epsilon(eps));
+    REQUIRE(Aminus2.Y() == doctest::Approx(0.f).epsilon(eps));
+    REQUIRE(Aminus2.Z() == doctest::Approx(1.f).epsilon(eps));
+    REQUIRE(Aminus2.W() == doctest::Approx(2.f).epsilon(eps));
+
+    Math::Vector4 Amul = A * 3;
+    REQUIRE(Amul.X() == doctest::Approx(3.f).epsilon(eps));
+    REQUIRE(Amul.Y() == doctest::Approx(6.f).epsilon(eps));
+    REQUIRE(Amul.Z() == doctest::Approx(9.f).epsilon(eps));
+    REQUIRE(Amul.W() == doctest::Approx(12.f).epsilon(eps));
+
+    Math::Vector4 Adiv = A / 2;
+    REQUIRE(Adiv.X() == doctest::Approx(0.5f).epsilon(eps));
+    REQUIRE(Adiv.Y() == doctest::Approx(1.f).epsilon(eps));
+    REQUIRE(Adiv.Z() == doctest::Approx(1.5f).epsilon(eps));
+    REQUIRE(Adiv.W() == doctest::Approx(2.f).epsilon(eps));
 }
